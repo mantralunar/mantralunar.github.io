51d4d4ed6db0e7143582d706c28b8d02689281b2
Mantralunar <39140384+mantralunar@users.noreply.github.com>
2023-12-28 13:53:34 +0000

    Update perfect_dark.sh

--- scripts/perfect_dark.sh
+++ scripts/perfect_dark.sh
@@ -1,17 +1,17 @@
-cd ~/Desktop/perfect_dark
+cd ~/Projects/perfect_dark
 rm -Rf build
 git pull
 
 make -f Makefile.port TARGET_PLATFORM=i686-linux
 
-mkdir -p ~/Desktop/perfect_dark/build/AppDir/usr/bin/
-mkdir -p ~/Desktop/perfect_dark/build/AppDir/usr/share/applications
-mkdir -p ~/Desktop/perfect_dark/build/AppDir/usr/share/icons/hicolor/512x512
-cp ~/Desktop/perfect_dark/build/ntsc-final-port/pd.exe ~/Desktop/perfect_dark/build/AppDir/usr/bin/pd
-curl https://raw.githubusercontent.com/mantralunar/mantralunar.github.io/main/img/pd.png -o ~/Desktop/perfect_dark/build/AppDir/usr/share/icons/hicolor/512x512/pd.png
+mkdir -p build/AppDir/usr/bin/
+mkdir -p build/AppDir/usr/share/applications
+mkdir -p build/AppDir/usr/share/icons/hicolor/512x512
+cp build/ntsc-final-port/pd.exe build/AppDir/usr/bin/pd
+curl https://raw.githubusercontent.com/mantralunar/mantralunar.github.io/main/img/pd.png -o build/AppDir/usr/share/icons/hicolor/512x512/pd.png
 
 
-cat > ~/Desktop/perfect_dark/build/AppDir/usr/share/applications/pd.desktop <<\EOF
+cat > build/AppDir/usr/share/applications/pd.desktop <<\EOF
 [Desktop Entry]
 Version=1.0
 Name=Perfect Dark
@@ -23,5 +23,5 @@ Categories=Game;
 X-AppImage-Integrate=false
 EOF
 
-
-env OUTPUT=~/Desktop/pd-x86_64.AppImage ~/linuxdeploy-x86_64.AppImage --appdir ~/Desktop/perfect_dark/build/AppDir/ --output appimage
+rm ~/Desktop/pd-x86_64.AppImage
+env OUTPUT=~/Desktop/pd-x86_64.AppImage ~/linuxdeploy-x86_64.AppImage --appdir build/AppDir/ --output appimage
