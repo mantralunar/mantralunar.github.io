92fe8e9aa5b6bc00927018c7b98188ac81c31840
Mantralunar <39140384+mantralunar@users.noreply.github.com>
2024-01-24 22:31:58 +0000

    Create OpenLara.sh

---
+++ scripts/OpenLara.sh
@@ -0,0 +1,29 @@
+cd ~/Projects/OpenLara/src/platform/nix
+./build.sh
+
+cd ../../../bin/
+mkdir -p AppDir/usr/share/applications/
+mkdir -p AppDir/usr/share/icons/hicolor/256x256/apps/
+cp ../src/platform/3ds/icon.png AppDir/usr/share/icons/hicolor/scalable/apps/OpenLara.png
+
+cat > AppDir/usr/share/applications/OpenLara.desktop <<\EOF
+[Desktop Entry]
+Version=1.0
+Name=Tomb Raider
+Exec=OpenLara
+Terminal=false
+Icon=OpenLara
+Type=Application
+Categories=Game;
+X-AppImage-Integrate=false
+EOF
+
+cat > AppDir/AppRun <<\EOF
+#!/bin/bash
+mkdir -p $HOME/.local/share/OpenLara/
+cd $HOME/.local/share/OpenLara
+${0%/*}/usr/bin/OpenLara
+EOF
+
+
+env OUTPUT=~/Desktop/OpenLara-x86_64.AppImage ~/linuxdeploy-x86_64.AppImage --appdir AppDir --executable OpenLara --output appimage
