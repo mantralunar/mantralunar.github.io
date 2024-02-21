e77da17f0aac28b8a59342af1d00642bc938701a
Mantralunar <39140384+mantralunar@users.noreply.github.com>
2024-02-21 22:48:32 +0000

    Create xash3d-fwgs.sh

---
+++ scripts/xash3d-fwgs.sh
@@ -0,0 +1,56 @@
+cd ~/Projects/HL2/
+rm -R /Projects/HL2/*
+ 
+
+git clone --recursive https://github.com/FWGS/hlsdk-portable
+cd hlsdk-portable
+cmake -DCMAKE_BUILD_TYPE=Release -D64BIT=1 -B build -S .
+cmake --build build
+
+cd ..
+
+git clone --recursive https://github.com/FWGS/xash3d-fwgs
+cd xash3d-fwgs
+./waf configure -T release -8
+./waf build
+./waf install --destdir=../AppDir/usr/bin
+
+cd ..
+mkdir -p AppDir/usr/bin/valve/cl_dlls/
+cp hlsdk-portable/build/cl_dll/client_amd64.so AppDir/usr/bin/valve/cl_dlls/client_amd64.so
+mkdir -p AppDir/usr/bin/valve/dlls/
+cp hlsdk-portable/build/dlls/hl_amd64.so AppDir/usr/bin/valve/dlls/hl_amd64.so
+
+
+mkdir -p AppDir/usr/share/applications
+mkdir -p AppDir/usr/share/icons/hicolor/128x128/apps/
+
+cp xash3d-fwgs/engine/platform/psvita/sce_sys/icon0.png AppDir/usr/share/icons/hicolor/128x128/apps/
+mv AppDir/usr/bin/*.so AppDir/usr/lib/
+
+
+cat > AppDir/usr/share/applications/xash3d.desktop <<EOF
+[Desktop Entry]
+Version=1.0
+Icon=icon0
+Exec=xash3d
+Terminal=false
+Type=Application
+Categories=Game
+Name=Half-Life
+GenericName=Half-Life
+Comment=Half-Life
+EOF
+
+cat > AppDir/AppRun <<\EOF
+#!/bin/sh
+mkdir -p $HOME/.local/share/xash3d/valve/cl_dlls
+mkdir -p $HOME/.local/share/xash3d/valve/dlls
+ln -sf ${0%/*}/usr/bin/valve/cl_dlls/client_amd64.so $HOME/.local/share/xash3d/valve/cl_dlls
+ln -sf ${0%/*}/usr/bin/valve/dlls/hl_amd64.so $HOME/.local/share/xash3d/valve/dlls
+ln -sf ${0%/*}/usr/bin/valve/extras.pk3 $HOME/.local/share/xash3d/valve/
+export XASH3D_BASEDIR=$HOME/.local/share/xash3d/
+${0%/*}/usr/bin/xash3d
+EOF
+
+env OUTPUT=~/Desktop/xash3d-x86_64.AppImage ~/linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
