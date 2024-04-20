c246f9aa2557667efd589708ba4ffb354b1c8b06
Mantralunar <39140384+mantralunar@users.noreply.github.com>
2024-04-20 13:59:30 +0100

    Update Sonic-1-2-2013-Decompilation.sh

--- scripts/Sonic-1-2-2013-Decompilation.sh
+++ scripts/Sonic-1-2-2013-Decompilation.sh
@@ -24,4 +24,4 @@ cd $HOME/.local/share/RetroEngine/Sonic1_RSDKv4/
 ${0%/*}/usr/bin/RSDKv4
 EOF
 
-env OUTPUT=~/Desktop/Sonic-1-2-2013-Decompilation-x86_64.AppImage ~/linuxdeploy-x86_64.AppImage --appdir build/AppDir/ --executable build/RSDKv4 --icon-file flatpak/com.sega.Sonic1.svg --plugin checkrt --output appimage
+~/linuxdeploy-x86_64.AppImage --appdir build/AppDir/ --executable build/RSDKv4 --icon-file flatpak/com.sega.Sonic1.svg --plugin checkrt --output appimage
