8b2b8065dfac718e1da583a479a330fbeda4b271
Mantralunar <39140384+mantralunar@users.noreply.github.com>
2024-04-21 20:17:08 +0100

    Update openbor.sh

--- scripts/openbor.sh
+++ scripts/openbor.sh
@@ -8,7 +8,6 @@ mkdir -p releases/AppDir/usr/bin
 mkdir -p releases/AppDir/usr/share/applications
 cp releases/LINUX_AMD64/OpenBOR/OpenBOR releases/AppDir/usr/bin
 
-#cat > releases/AppDir/OpenBOR.desktop <<\EOF
 cat > releases/AppDir/usr/share/applications/OpenBOR.desktop <<\EOF
 [Desktop Entry]
 Version=1.0
@@ -21,7 +20,6 @@ Categories=Game;
 X-AppImage-Integrate=false
 EOF
 
-#cp resources/OpenBOR_Icon_128x128.png releases/AppDir/
 
 cat > releases/AppDir/AppRun <<\EOF
 #!/bin/bash
@@ -32,11 +30,8 @@ rm -rf $HOME/.local/share/OpenBOR/Screenshots/*
 rm -rf $HOME/.local/share/OpenBOR/Translation/*
 
 cd $HOME/.local/share/OpenBOR
-export LD_LIBRARY_PATH="${APPDIR}/usr/lib:${LD_LIBRARY_PATH}"
 ${0%/*}/usr/bin/OpenBOR
 EOF
 
-#chmod +x releases/AppDir/AppRun
 
-#appimagetool-x86_64.AppImage releases/AppDir/ ~/Desktop/openbor-x86_64.AppImage
 linuxdeploy-x86_64.AppImage --appdir releases/AppDir/ --icon-file resources/OpenBOR_Icon_128x128.png --output appimage
